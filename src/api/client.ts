38c789c5901af7c38232e4cb05260c04b15bb46a
Applefox-key <applefox.key@gmail.com>
2026-03-19 20:17:44 -0500

    fix: mock server -> real server

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const BASE_URL = 'https://api.learnapp.pro'
-export const BASE_URL = "http://localhost:8000";
+const BASE_URL = 'https://api.learnapp.pro'
+// export const BASE_URL = "http://localhost:8000";
 export const TOKEN_KEY = 'tokentracker'
 
 export const apiClient = axios.create({
